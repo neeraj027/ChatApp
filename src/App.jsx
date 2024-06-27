ef7aed7bca0a32caf070d584dbfd8afdb953e052
neeraj027 <neerajkshatriy@gmail.com>
2024-06-28 01:39:11 +0530

    Added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -8,31 +8,128 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import Message from "./Components/Message";
+import {
+  onAuthStateChanged,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { app } from "./firebase.js";
+
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+} from "firebase/firestore";
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider);
+};
+
+const logoutHandler = () => {
+  signOut(auth);
+};
 
 const App = () => {
+  const [user, setUser] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "Messages"), {
+        text: msg,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp(),
+      });
+
+      setMsg("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (data) => {
+      setUser(data);
+    });
+
+    onSnapshot(collection(db, "Messages"), (snap) => {
+      snap.docs.map((item) => {
+        const id = item.id;
+        return { id, ...item.data() };
+      });
+    });
+  }, []);
+
   return (
     <Box bg={"green.500"}>
-      <Container bg={"white"} height={"100vh"}>
-        <VStack h={"full"} padding={"4"}>
-          <Button w={"full"} colorScheme="red">
-            Logout
-          </Button>
-          <VStack h={"full"} w={"full"}>
-            <Message text={"hello"} />
-          </VStack>
+      {user ? (
+        <Container bg={"white"} height={"100vh"}>
+          <VStack h={"full"} padding={"4"}>
+            <Button onClick={logoutHandler} w={"full"} colorScheme="red">
+              Logout
+            </Button>
+            <VStack
+              flexDirection={"column-reverse"}
+              overflowY={"auto"}
+              h={"full"}
+              w={"full"}
+            >
+              {messages.map((item, index) => {
+                <Message
+                  key={index}
+                  user={item.uri === user.uid ? "me" : ""}
+                  text={item.text}
+                  uri={item.uri}
+                />;
+              })}
+            </VStack>
 
-          <form action="" style={{ width: "100%" }}>
-            <HStack>
-              <Input
-                w={"full"}
-                placeholder="Message"
-                border={"2px solid blue"}
-              ></Input>
-              <Button colorScheme="purple">Send</Button>
-            </HStack>
-          </form>
+            <form
+              onSubmit={
+                msg === ""
+                  ? (e) => {
+                      e.preventDefault();
+                    }
+                  : submitHandler
+              }
+              style={{ width: "100%" }}
+            >
+              <HStack>
+                <Input
+                  value={msg}
+                  onChange={(e) => {
+                    setMsg(e.target.value);
+                  }}
+                  w={"full"}
+                  placeholder="Message"
+                  border={"2px solid blue"}
+                ></Input>
+                <Button type="submit" colorScheme="purple">
+                  Send
+                </Button>
+              </HStack>
+            </form>
+          </VStack>
+        </Container>
+      ) : (
+        <VStack justifyContent={"center"} h={"100vh"}>
+          <Button onClick={loginHandler}>Sign in with Google</Button>
         </VStack>
-      </Container>
+      )}
     </Box>
   );
 };
--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,10 +1,46 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { Avatar, HStack, Text } from "@chakra-ui/react";
 import React from "react";
 
-const Message = ({ text }) => {
+const Message = ({ text, user = "other", uri }) => {
   return (
-    <HStack>
-      <Text>{text}</Text>
+    <HStack
+      maxWidth={"300"}
+      alignSelf={user === "me" ? "flex-start" : "flex-end"}
+      bg={"gray.300"}
+      paddingY={"2"}
+      paddingX={"4"}
+      borderBottomRightRadius={user === "me" ? "12" : ""}
+      borderBottomLeftRadius={user === "me" ? "" : "12"}
+      borderTopRadius={"12"}
+      position={"relative"}
+    >
+      {user === "me" && (
+        <Avatar
+          position={"absolute"}
+          bottom={"1"}
+          left={2}
+          w={"8"}
+          h={"8"}
+        ></Avatar>
+      )}
+
+      <Text
+        marginLeft={user === "me" ? "8" : ""}
+        marginRight={user === "me" ? "" : "8"}
+        fontWeight={"500"}
+      >
+        {text}
+      </Text>
+
+      {user === "other" && (
+        <Avatar
+          position={"absolute"}
+          bottom={"1"}
+          right={2}
+          w={"8"}
+          h={"8"}
+        ></Avatar>
+      )}
     </HStack>
   );
 };
